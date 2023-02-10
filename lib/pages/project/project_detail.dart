64b3bf280de4034c4c3dbb1387cc7de198d32dc7
Shadman Sakib <shadman@felicity.digital>
2023-02-10 17:02:14 +0600

    Add Details Page

---
+++ lib/pages/project/project_detail.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/models/project.dart';
+
+class ProjectDetail extends StatelessWidget {
+  final Project project;
+  const ProjectDetail({
+    required this.project,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: SizedBox(
+          width: screenSize.width,
+          height: screenSize.height,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      BackButton(
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      Text(
+                        project.name,
+                        style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text(
+                          project.year.toString(),
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
